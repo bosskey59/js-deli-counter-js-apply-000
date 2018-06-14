a097ce4129ba2fbbae97f3030495f754305da5f6
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-14 18:08:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
   let number = katzDeliLine.length ;
-  return "Welcome, " +name+ ". You are number "+ number +" in line.";
+  return "Welcome, " + name + ". You are number "+ number +" in line.";
+}
+
+  let counter = 0;
+  
+function takeANumber_2(line){
+    line.push(counter)
+    counter++;
+    return "You are customer  number:" + counter;
 }
 
 function nowServing(line){
@@ -15,15 +23,15 @@ function nowServing(line){
 }
 
 function currentLine(line){
-  if (line.length=== 0){
+  if (line.length === 0){
     return "The line is currently empty."
   }
     
   else{
     let array = [];
       for (let x=0; x<line.length; x++){
-       let numberOfPerson = x+1;
-       array.push ( " "+ numberOfPerson +". "+ line[x] );
+       //let numberOfPerson = x+1;
+       array.push ( " "+ (x+1) +". "+ line[x] );
      }
      return "The line is currently:"+ array;
   }
